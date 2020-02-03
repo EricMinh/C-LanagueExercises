108f91d85de284023f0437f22711eb0bac65e298
EricMinh <32127944+EricMinh@users.noreply.github.com>
2020-02-02 23:30:23 -0800

    Create ex14.cpp

---
+++ ex14.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define CHARTOTAL 127
+
+int main(int argc, const char * argv[]) {
+
+	int c, a, histo[CHARTOTAL];
+
+	for (a = 0; a < CHARTOTAL; a++) {
+
+		histo[a] = 0;
+
+	}
+
+	while ((c = getchar()) != EOF) {
+
+		if (c <= CHARTOTAL ) {
+
+			histo[c]++;
+
+		}
+
+	}
+
+	for (a = 0; a < CHARTOTAL; a++) {
+
+		if (isprint(a)) {
+
+			printf("%3d | %c (%d): ", a, a, histo[a]);
+
+		}
+		else {
+
+			printf("%3d |   (%d): ", a, histo[a]);
+
+		}
+
+		while (histo[a] > 0) {
+
+			printf("*");
+			histo[a]--;
+
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
